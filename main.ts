6920d91490fa08488f538918a5f286955770da11
caiosouzafunec-cell <caio.souza.funec@gmail.com>
2025-09-30 08:26:21 -0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -8,6 +8,12 @@ const view = new AlunoView();
 const controller = new AlunoController(model, view);
 
 // Simulação de uso
-controller.cadastrarAluno(1, "Seu Nome", 18);
-
+controller.cadastrarAluno(1, "Pedro", 16);
+controller.cadastrarAluno(2, "Sophia", 18);
+controller.cadastrarAluno(3, "Luan", 15);
+controller.cadastrarAluno(4, "Bruno", 20);
+controller.cadastrarAluno(5, "Rita", 22);
+controller.cadastrarAluno(6, "Lucas", 19);
+controller.cadastrarAluno(7, "Maria", 17);
 controller.exibirAlunos();
+
